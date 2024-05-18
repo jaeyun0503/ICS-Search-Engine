ce6d824720c8b89e0d140ab336b62f8dc0cffbb5
Jae Yun Kim <jaeyk14@uci.edu>
2024-05-17 19:15:42 -0700

    chat we cooked

--- search.py
+++ search.py
@@ -40,7 +40,7 @@ def tokenize_query(query):
 
 #     return sorted_docs[:top_k]
 
-def calculate_tfidf(query, top_k=5):
+def calculate_tfidf(query, top_k=20):
     """
     Calculate TF-IDF scores for query matching documents
     """
@@ -53,6 +53,7 @@ def calculate_tfidf(query, top_k=5):
                 index = reference_index[token]
                 file.seek(index)
                 data = pickle.load(file) #use pickle.load
+                print(data)
                 postings = data[token]
                 for doc_id, info in postings.items():
                     if doc_id != "document_frequency":
@@ -62,6 +63,49 @@ def calculate_tfidf(query, top_k=5):
     sorted_docs = sorted(doc_scores.items(), key=lambda item: item[1], reverse=True)
 
     return sorted_docs[:top_k]
+
+
+# def calculate_tfidf(query, top_k=20):
+#     """
+#     Calculate TF-IDF scores for query matching documents.
+#     Only documents containing all tokens in the query are considered valid.
+#     """
+#     N = DOCUMENTS
+#     doc_scores = defaultdict(float)
+#     valid_documents = None  # To track intersection of document IDs across tokens
+
+#     with open("./res/inverted_index.pkl", "rb") as file:
+#         for token in query:
+#             if token in reference_index:
+#                 index = reference_index[token]
+#                 file.seek(index)
+#                 data = pickle.load(file)  # Load the token data
+#                 print(data)
+#                 postings = data[token]
+                
+#                 current_docs = set()  # Track current token document IDs
+                
+#                 # Collect document IDs and their scores if token_frequency is not zero
+#                 for doc_id, info in postings.items():
+#                     if doc_id != "document_frequency" and info["token_frequency"] > 0:
+#                         current_docs.add(doc_id)
+#                         doc_scores[doc_id] += info["token_frequency"]
+
+#                 # Update valid_documents with the intersection of current_docs
+#                 if valid_documents is None:
+#                     valid_documents = current_docs
+#                 else:
+#                     print(valid_documents, current_docs)
+#                     valid_documents.intersection_update(current_docs)
+
+#     # Filter out scores for documents that do not contain all tokens
+#     final_scores = {doc_id: score for doc_id, score in doc_scores.items() if doc_id in valid_documents}
+
+#     # Sort documents by their TF-IDF score
+#     sorted_docs = sorted(final_scores.items(), key=lambda item: item[1], reverse=True)
+    
+#     return sorted_docs[:top_k]
+
 
 
 def search():
@@ -88,13 +132,19 @@ def search():
         # find common doc id btwn the tokens 
         # rank them ?? 
         # map the doc id(s) back to the url(s)
+        return_list = []
+        for i in range(len(sorted_docs)):
+            if urls[sorted_docs[i][0]] not in return_list:
+                return_list.append(urls[sorted_docs[i][0]])
+            if len(return_list) == 5:
+                break
 
         print("\nTop 5 URLs:")
 
-        for i in sorted_docs:
-            print(urls[i[0]])
+        for i in return_list:
+            print(i)
         
-        print("\Search time:", time.time() - t, "seconds")
+        print("\nSearch time:", time.time() - t, "seconds")
 
 
 if __name__ == "__main__":
